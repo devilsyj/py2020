b66cb078160245aeb8faee3deae4e38507e5c0e3
syj <devilsyj@me.com>
2020-03-01 12:27:30 +0800

    update0301

--- class_1.py
+++ class_1.py
@@ -1,13 +1,30 @@
 #coding:UTF-8
+import inspect
 class Fruit:
     price = 0    #类属性(静态变量，可以直接被类调用不需实例化)，类和实例都可以访问
 
     def __init__(self):
         self.__color = 'red'   #实例属性，访问必须先实例化
+        self.num = 100
+
+    def funcname(self):
+        return inspect.stack()[1][3]
+        print '2020'
+
+    def test(self):
+        print self.funcname()
 
 
 
 if __name__ ==  "__main__":
+    print '111'
+    apple = Fruit()
+    apple.test()
+    # print dir(Fruit)
+    # print apple
+    # n = getattr(apple,'test')
+    # print type(n)
+    # n()
 
     # print Fruit.price
     # apple = Fruit()
@@ -16,8 +33,11 @@ if __name__ ==  "__main__":
     # print Fruit.price, apple.price
     # banana = Fruit()
     # print banana.price
-    apple = Fruit()
+    # dir(Fruit)
+    # apple = Fruit()
     # print apple.__color    #私有属性不能直接通过实例化访问
-    print dir(Fruit())
-    print apple._Fruit__color
+    # print dir(Fruit())
+    # print apple._Fruit__color
+    # apple._Fruit__dispay()
+    # n = getattr(apple,dis)
 
